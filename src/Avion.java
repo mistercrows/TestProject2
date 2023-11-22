83979a95252d77c9769a7b50a12896cc8e74b0a2
Jonathan Gervais-Laporte <j.gervaislaporte@outlook.com>
2023-11-22 08:52:43 -0500

    test

---
+++ src/Avion.java
@@ -0,0 +1,11 @@
+public class Avion {
+    private int x;
+
+    public Avion(){
+        x = 10;
+    }
+
+    public int getX(){
+        return x;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Avion avion1 = new Avion();
+        System.out.println(avion1.getX());
+    }
+}
